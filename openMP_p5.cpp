945e649bca6128b333a4822f81d3d1436866bf01
anurag <anuragv.1020@gmail.com>
2021-12-13 00:51:39 +0530

    updated p5

--- openMP_p5.cpp
+++ openMP_p5.cpp
@@ -11,24 +11,32 @@ int main()
 {
 	double *fib = (double*)malloc(N * sizeof(double));
 
-	omp_set_num_threads(omp_get_max_threads());
+
 
 	fib[0] = 0.0;
 
 	fib[1] = 1.0;
-	#pragma omp parallel
+	#pragma omp parallel shared(fib) num_threads(2)
 	{
-		for (int i = 2; i < N; i++) {
-			fib[i] = fib[i - 1] + fib[i - 2];
-			//printf("Thread id : %d\n", omp_get_thread_num());
+		#pragma omp single
+		{
+			printf("\nThread id : %d Calculating fib series.... \n", omp_get_thread_num());
+			for (int i = 2; i < N; i++) {
+				fib[i] = fib[i - 1] + fib[i - 2];
+			}
+		}
+		#pragma omp barrier
+		{
+			#pragma omp single
+			{
+				printf("\nThread id : %d printing fib series... \n", omp_get_thread_num());
+				printf("Fib series.. \n");
+				for (int i = 0; i < N; i++)
+					printf("%0.0f ", fib[i]);
+
+			}
 		}
 	}
-	#pragma omp barrier
-	#pragma omp single
-	{
-		printf("Fib series.. \n");
-		for (int i = 0; i < N; i++)
-			printf("%0.0f ", fib[i]);
-	}
+	
 	return 0;
 }
